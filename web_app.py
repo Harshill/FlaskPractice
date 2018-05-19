d082cf1effa5cdbb712ac12b77d016bf4b2610e4
Harshill <harshill@my.yorku.ca>
2018-05-19 13:12:12 -0400

    First website attempt

---
+++ web_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for
+
+app = Flask('__name__')
+
+posts = [
+    {
+        'author': 'Steve',
+        'title': 'Hello World',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    }
+]
+
+
+@app.route('/')
+def hello():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
